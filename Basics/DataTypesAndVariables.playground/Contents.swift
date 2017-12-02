fe86fb3ec01cf12e030a979d23e37826eb2b95d2
Varun <varun.kudalkar@gmail.com>
2017-12-02 11:26:50 +0530

    DataTypes and variables

---
+++ Basics/DataTypesAndVariables.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var a : String
+
+a = "20"
+
+var b = 105
+
+a = "value of b is \(a)"
+
+print(a)
+
+var c = 10.20
+
+var d = true
+
+d = false
+    
+print("Value of c : \(d)")
---
+++ Basics/FirstDemo.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var strNew = "Welcome to CodeKul"
+
+print(str)
+
+print(strNew)
+
+
